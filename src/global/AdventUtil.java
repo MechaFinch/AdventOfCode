5397fdba8b4a85618e324faef6c44308181ebe23
Mechafinch <wetcathunder@gmail.com>
2020-12-01 20:10:57 -0500

    Day 1

---
+++ src/global/AdventUtil.java
@@ -0,0 +1,86 @@
+package global;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Class for general utilities
+ * 
+ * @author Mechafinch
+ */
+public class AdventUtil {
+	/**
+	 * Reads the input file as an arraylist of strings
+	 * 
+	 * @param f
+	 * @return
+	 * @throws IOException
+	 */
+	public static ArrayList<String> inputList(File f) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		ArrayList<String> s = (ArrayList<String>) br.lines().collect(Collectors.toList());
+		br.close();
+		return s;
+	}
+	
+	/**
+	 * Reads the input file as an arraylist of ints
+	 * 
+	 * @param f
+	 * @return
+	 * @throws IOException
+	 */
+	public static ArrayList<Integer> intList(File f) throws IOException {
+		return (ArrayList<Integer>) intStream(f).boxed().collect(Collectors.toList());
+	}
+	
+	/**
+	 * Reads the input file as a stream of strings
+	 * 
+	 * @param f
+	 * @return
+	 * @throws IOException
+	 */
+	public static Stream<String> stringStream(File f) throws IOException {
+		return inputList(f).stream();
+	}
+	
+	/**
+	 * Reads the input file as a stream of ints
+	 * 
+	 * @param f
+	 * @return
+	 * @throws IOException
+	 */
+	public static IntStream intStream(File f) throws IOException {
+		return stringStream(f).mapToInt(s -> Integer.parseInt(s));
+	}
+	
+	/**
+	 * Reads the input file as an array of strings
+	 * 
+	 * @param f
+	 * @return
+	 * @throws IOException
+	 */
+	public static String[] inputLines(File f) throws IOException {
+		return inputList(f).toArray(new String[0]);
+	}
+	
+	/**
+	 * Reads the input file as an array of ints
+	 * 
+	 * @param f
+	 * @return
+	 * @throws IOException
+	 */
+	public static int[] inputInts(File f) throws IOException {
+		return intStream(f).toArray();
+	}
+}
---
+++ src/year2020/Day01.java
@@ -0,0 +1,74 @@
+package year2020;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import global.AdventUtil;
+
+public class Day01 {
+	public static void main(String[] args) throws IOException{
+		File inputfile = new File("input.txt");
+		
+		ArrayList<Integer> input = AdventUtil.intList(inputfile);
+		input.sort((a, b) -> b - a);
+		System.out.println(input);
+		
+		// Search recursively
+		System.out.println(recursiveHelper(input, 0, 8, 0));
+	}
+	
+	/**
+	 * A completely overkill method which can search for arbitrarily sized sets, currently setup for 8 values (which works!)
+	 * 
+	 * @param in
+	 * @param startIndex
+	 * @param depth
+	 * @param sum
+	 * @return
+	 */
+	public static int recursiveHelper(ArrayList<Integer> in, int startIndex, int depth, int sum) {
+		// Last number
+		if(depth == 1) {
+			for(int i = startIndex; i < in.size(); i++) {
+				if(in.get(i) + sum == 2020) {
+					System.out.println("\t\t\t\t\t\t\t" + in.get(i));
+					return in.get(i);
+				} else if(in.get(i) + sum < 2020) { // too far
+					return -1;
+				}
+			}
+			
+			return -1;
+		}
+		
+		// Find largest value whose sum is less than 2020
+		int i;
+		for(i = startIndex; i < in.size(); i++) {
+			if(in.get(i) + sum < 2020) {
+				break;
+			}
+		}
+		
+		if(i == in.size()) return -1;
+		
+		// Traverse smaller numbers
+		for(; i < in.size(); i++) {
+			// Print values we're using
+			for(int j = 8 - depth; j > 0; j--) {
+				System.out.print("\t");
+			}
+			
+			System.out.println(in.get(i));
+			
+			// Recurse
+			int r = recursiveHelper(in, i, depth - 1, sum + in.get(i));
+			if(r != -1) {
+				return in.get(i) * r;
+			}
+		}
+		
+		// Not found
+		return -1;
+	}
+}
