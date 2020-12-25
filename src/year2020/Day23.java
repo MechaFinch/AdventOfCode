5f830129515c0150c348da6e978bc1dc1c88f71d
Mechafinch <wetcathunder@gmail.com>
2020-12-25 14:38:50 -0500

    2020 Day 25
    Got day 23 running in a couple seconds with the magic of linked lists

--- src/year2020/Day23.java
+++ src/year2020/Day23.java
@@ -2,6 +2,8 @@ package year2020;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,7 +14,7 @@ public class Day23 {
 	public static void main(String[] args) throws IOException {
 		long startTime = System.nanoTime();
 		
-		System.out.println(part1(AdventUtil.inputLines(AdventUtil.DEFAULT)[0]));
+		System.out.println(part2(AdventUtil.inputLines(AdventUtil.DEFAULT)[0]));
 		
 		System.out.println((System.nanoTime() - startTime) / 1_000_000 + " ms");
 	}
@@ -32,13 +34,122 @@ public class Day23 {
 		}
 		
 		// Run
-		run(cups, 1_000_000, 10_000);
+		run_fast(cups, 1_000_000, 10_000_000);
+		
+		System.out.println(new HashSet<Integer>(cups).size()); // Will be 1_000_000 if there's no pointer duplication
 		
 		int i = cups.indexOf(1);
+		// System.out.println(cups);
 		System.out.println(i);
 		System.out.println(cups.subList(i, i + 4));
 		
 		return ((long) cups.get(i + 1)) * ((long) cups.get(i + 2));
+	}
+	
+	/**
+	 * Runs the game but fast
+	 * 
+	 * @param cups
+	 * @param max
+	 * @param steps
+	 */
+	public static void run_fast(List<Integer> cups, int max, int steps) {
+		// Hashmap based linked list because idk
+		// Hashmap key is the value, hasmap list has {previous, next}
+		HashMap<Integer, IntPair> linkedList = new HashMap<>(max);
+		
+		System.out.println("Building linked list...");
+		
+		// Convert to linked list
+		for(int i = 0; i < cups.size(); i++) {
+			// Original group
+			if(i < 9) {
+				int prev = (i == 0) ? max - 1 : cups.get(i - 1) - 1,
+					next = (i == 8) ? 9 : cups.get(i + 1) - 1;
+				linkedList.put(cups.get(i) - 1, new IntPair(prev, next));
+			} else if(i == max - 1) { // Wrap properly
+				linkedList.put(i, new IntPair(i - 1, cups.get(0) - 1));
+			} else { // Linear
+				linkedList.put(i, new IntPair(i - 1, i + 1));
+			}
+		}
+		
+		System.out.println("Linked List built. Running steps...");
+		
+		// Run steps
+		List<Integer> picked = new ArrayList<>();
+		
+		// Step, current pointer
+		for(int s = 0, cp = cups.get(0) - 1; s < steps; s++) {
+			// System.out.println("\n" + s);
+			// printLinkedList(linkedList);
+			
+			// Assemble picked list
+			picked.clear();
+			int p = cp;
+			for(int i = 0; i < 3; i++) {
+				// Next value
+				p = linkedList.get(p).getB();
+				picked.add(p);
+			}
+			
+			// Find destination
+			int dest = (cp == 0 ? max - 1 : cp - 1);
+			while(picked.contains(dest)) dest = (dest == 0 ? max - 1 : dest - 1);
+			
+			// Find new linkings
+			int nextCP = linkedList.get(p).getB(),			// CP will link to the value after the last picked
+				nextDest = picked.get(0),					// The destination will link to picked[0]
+				nextPicked = linkedList.get(dest).getB();	// picked[2] will link to the value after dest
+			
+			// System.out.println((cp + 1) + "   " + (dest + 1) + "   " + picked);
+			// System.out.println((nextCP + 1) + "   " + (nextDest + 1) + "   " + (nextPicked + 1));
+			
+			if(cp == nextCP) throw new IllegalStateException("Bad CP  " + s);
+			if(dest == nextDest) throw new IllegalStateException("Bad dest  " + s);
+			if(picked.get(2) == nextPicked) throw new IllegalStateException("Bad picked  " + s);
+			
+			// Update previous values
+			linkedList.get(nextCP).setA(cp);
+			linkedList.get(nextDest).setA(dest);
+			linkedList.get(nextPicked).setA(picked.get(2));
+			
+			// Update next values
+			linkedList.get(cp).setB(nextCP);
+			linkedList.get(dest).setB(nextDest);
+			linkedList.get(picked.get(2)).setB(nextPicked);
+			
+			// Update cp
+			cp = linkedList.get(cp).getB();
+		}
+		
+		System.out.println("Steps complete. Converting linked list...");
+		
+		// Convert back to list starting from 1
+		for(int i = 0, p = 0; i < linkedList.size(); i++) {
+			cups.set(i, p + 1);
+			p = linkedList.get(p).getB();
+		}
+		
+		System.out.println("Complete.\n");
+	}
+	
+	/**
+	 * Prints in linked order
+	 * 
+	 * @param linkedList
+	 */
+	public static void printLinkedList(HashMap<Integer, IntPair> linkedList) {
+		List<Integer> toPrint = new ArrayList<>();
+		
+		// Convert to list starting from 1
+		for(int i = 0, p = 0; i < linkedList.size(); i++) {
+			toPrint.add(p + 1);
+			if(!linkedList.containsKey(p)) break;
+			p = linkedList.get(p).getB();
+		}
+		
+		System.out.println(toPrint);
 	}
 	
 	/**
@@ -51,7 +162,7 @@ public class Day23 {
 		// Construct list
 		List<Integer> cups = new ArrayList<>(input.chars().map(x -> x - '0').boxed().collect(Collectors.toList()));
 		
-		run(cups, 9, 100);
+		run_slow(cups, 9, 100);
 		
 		// Assemble answer
 		int ans = 0,
@@ -71,7 +182,7 @@ public class Day23 {
 	 * @param max Maximum value
 	 * @param steps Number of steps to run
 	 */
-	public static void run(List<Integer> cups, int max, int steps) {
+	public static void run_slow(List<Integer> cups, int max, int steps) {
 		List<Integer> picked = new ArrayList<>();
 		
 		// Run steps
@@ -109,3 +220,25 @@ public class Day23 {
 		}
 	}
 }
+
+// A pair of ints
+class IntPair {
+	int a, b;
+	
+	public IntPair(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public void setA(int na) { a = na; }
+	public void setB(int nb) { b = nb; }
+	
+	public int getA() { return a; }
+	public int getB() { return b; }
+	
+	@Override
+	public String toString() {
+		return "(" + a + ", " + b + ")";
+	}
+}
+
---
+++ src/year2020/Day25.java
@@ -0,0 +1,62 @@
+package year2020;
+
+import java.io.IOException;
+
+import global.AdventUtil;
+
+public class Day25 {
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println(part1(AdventUtil.inputLines(AdventUtil.DEFAULT)));
+	}
+	
+	/**
+	 * Part 1
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public static long part1(String[] input) {
+		long pk1 = Long.parseLong(input[0]),
+			 pk2 = Long.parseLong(input[1]),
+			 size = findLoopSize(pk2, 7, 20201227);
+		
+		System.out.println(findLoopSize(pk1, 7, 20201227));
+		System.out.println(findLoopSize(pk2, 7, 20201227));
+		
+		return getKey(1, pk1, 20201227, size);
+	}
+	
+	/**
+	 * Finds the loop size of a key
+	 * 
+	 * @param key
+	 * @param subject
+	 * @param divisor
+	 * @return
+	 */
+	public static long findLoopSize(long key, long subject, long divisor) {
+		long size = 0,
+			 value = 1;
+		
+		while(value != key) {
+			value = (value * subject) % divisor;
+			size++;
+		}
+		
+		return size;
+	}
+	
+	/**
+	 * Runs the encryption
+	 * 
+	 * @param value
+	 * @param subject
+	 * @param divisor
+	 * @return
+	 */
+	public static long getKey(long value, long subject, long divisor, long size) {
+		for(int i = 0; i < size; i++) value = (value * subject) % divisor;
+		return value;
+	}
+}
